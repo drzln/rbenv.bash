ed107fd26f557b8f2ef386f83148d221a552c3fe
Luis De Siqueira <luis@pleme.io>
2022-12-23 00:47:53 -0300

    init

--- rbenv.bash
+++ rbenv.bash
@@ -2,21 +2,31 @@
 # rbenv helper functions
 ################################################################################
 
+__rbenv_home="$HOME/.rbenv"
+__rbenv_versions="$__rbenv_home/versions"
+__rbenv_bin="$__rbenv_home/bin/rbenv"
+__rbenv_ruby_version=$(cat .ruby-version)
+__rbenv_ruby_gemset=$(cat .ruby-gemset)
+__rbenv_gemsets="$__rbenv_versions/$__rbenv_ruby_version/gemsets"
+__rbenv_gemset="$__rbenv_versions/$__rbenv_ruby_version/gemsets/$__rbenv_ruby_gemset"
+__rbenv_yard="$__rbenv_gemset/bin/yard"
+__rbenv_yard_doc="$__rbenv_gemset/doc"
+
 provision:rbenv:version() {
-	if [ -f ~/.rbenv/bin/rbenv ]; then
-		! [ -d "$HOME/.rbenv/versions/$(cat .ruby-version)" ] && rbenv install
+	if [ -f $__rbenv_bin ]; then
+		! [ -d "$__rbenv_versions/$__rbenv_ruby_version" ] && $__rbenv_bin install
 	fi
 }
 
 provision:rbenv:gemset() {
 	if [ -f Gemfile ]; then
-		! [ -d "$HOME/.rbenv/versions/$(cat .ruby-version)/gemsets/$(cat .ruby-gemset)" ] && gem install bundler && bundle
+		! [ -d "$__rbenv_gemset" ] && gem install bundler && bundle
 	fi
 }
 
 provision:rbenv:yard:gems() {
-	if [ -f "$HOME/.rbenv/versions/$(cat .ruby-version)/gemsets/$(cat .ruby-gemset)/bin/yard" ]; then
-		! [ -d "$HOME/.rbenv/versions/$(cat .ruby-version)/gemsets/$(cat .ruby-gemset)/doc" ] && yard gems
+	if [ -f "$__rbenv_yard" ]; then
+		! [ -d "$__rbenv_yard_doc" ] && yard gems
 	fi
 }
 
